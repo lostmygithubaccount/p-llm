85700dc304db57ffcc6d295204e6d3e94077b132
Cody <cody@dkdc.dev>
2023-06-24 16:25:03 -0700

    qs

--- run.py
+++ run.py
@@ -1,25 +1,16 @@
 # imports
-import os
+prompt = "You are an AI assitant tasked with outputting a single"
+# imports
 import ibis
 import torch
 import random
-import requests
 
 import numpy as np
 import pandas as pd
 
 from ibis import _
-from dotenv import load_dotenv
 from transformers import pipeline
 from ibis.expr.operations import udf
-
-# load env variables
-load_dotenv()
-OPENAI_KEY = os.getenv("OPENAI_KEY")
-
-# openai settings
-url = "https://api.openai.com/v1/chat/completions"
-model = "gpt-3.5-turbo"
 
 # configure ibis
 ibis.options.interactive = True
